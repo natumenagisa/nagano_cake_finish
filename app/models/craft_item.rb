5eff2f1f20e2ddb90299f6b1adf7c01fb953b833
natumenagisa <105056597+natumenagisa@users.noreply.github.com>
2022-07-07 06:52:35 +0000

    first commit

---
+++ app/models/craft_item.rb
@@ -0,0 +1,2 @@
+class CraftItem < ApplicationRecord
+end
---
+++ test/models/craft_item_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CraftItemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
